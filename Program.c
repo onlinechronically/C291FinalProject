2e1d3c77b3c32142d8b021f7ce9b59b25dd81254
lillie <onlinechronically@protonmail.com>
2024-04-25 18:18:34 -0400

    rename: c program+a lot of changes

--- Program.c
+++ Program.c
@@ -15,8 +15,11 @@ void load_data_files();
 void display_game_data();
 char *correctAnswer(char *optionOne, char *optionTwo, char *optionThree, char *optionFour, int correctOption);
 void run_level();
+char *get_card(int card);
+void blackjack();
 void intermission();
 void clear_console();
+void draw_blackjack_logo();
 void display_logo();
 void draw_skull();
 void do_death();
@@ -66,17 +69,22 @@ void show_start_screen() {
 
 // This method handles the command line input the user should enter on the start screen
 void handle_start_option() {
-    int opt;
+    for (int i = 0; i < 52; i++) get_card(i);
+    blackjack();
+    /*int opt;
     scanf("%d", &opt);
     if (opt == 1) {
         create_new_session();
         run_level();
     } else if (opt == 2) {
-        load_save_file("");
+        char fileName[100];
+        printf("Enter the file name of the you would like to load: ");
+        fgets(fileName, 100, stdin);
+        load_save_file(fileName);
         run_level();
     } else if (opt == 3) {
         exit(1);
-    }
+    }*/
 }
 
 // This method saves the current game to a file
@@ -95,7 +103,7 @@ void create_save() {
 
 // This method loads a game session from a file
 void load_save_file(char *fileName) {
-    FILE *fp;
+    //FILE *fp;
     printf("%s", fileName);
 }
 
@@ -244,13 +252,16 @@ void run_level() {
     if (!isAQuest) {
         puts("You happen to come across a somewhat interesting travelling market!!!");
         puts("The travelling salesperson has a few offers for you:");
+        printf("(0) %s: Free\n", "A risky but rewarding gambling game");
         if (session.balance >= 10) printf("(1) %s: $%d\n", "A suprisingly but promising healing potion", 10);
         if (session.balance >= 2) printf("(2) %s: $%d\n", "A random (but good to know) fact", 2);
         if (session.balance >= 1) printf("(3) %s: $%d\n", "A mysterious magic mushroom", 1);
         int opt;
         printf("What would you like to buy (-1 to leave): ");
         scanf("%d", &opt);
-        if (opt == 1) {
+        if (opt == 0) {
+
+        } else if (opt == 1) {
             replenish_health();
         } else if (opt == 2 && session.balance >= 2) {
             printf("Here's a good tip: %s\n", facts[rand() % totalFacts]);
@@ -297,6 +308,114 @@ void run_level() {
     }
 }
 
+char *get_card(int card) {
+    int newCard = card + 1;
+    char *values[] = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+    char *suits[] = {"Clubs", "Hearts", "Spades", "Diamonds"};
+    printf("Card #: %s | Suit: %s | num: %d\n", values[newCard % 13], suits[card / 13], card);
+    char *cardName = malloc(strlen(values[newCard % 13]) + strlen(suits[card / 13]) + 3 + 1);
+    if (cardName == NULL) {
+        puts("There was an error allocating memory");
+        exit(1);
+    }
+    size_t i;
+    for (i = 0; i < strlen(values[newCard % 13]); i++) {
+        cardName[i] = values[newCard % 13][i];
+    }
+    cardName[i++] = ' ';
+    cardName[i++] = '(';
+    for (i = i; i < strlen(values[newCard % 13]) + 2 + strlen(suits[card / 13]); i++) {
+        cardName[i] = suits[card / 13][i - strlen(values[newCard % 13]) - 2];
+    }
+    cardName[i++] = ')';
+    cardName[i++] = '\0';
+    return cardName;
+}
+
+int *get_total_points(int *values, int totalValues) {
+    int *allValues = calloc(1, sizeof(int));
+    int mainTotal = 0;
+    int areAcesFine = 1;
+    for (int i = 0; i < totalValues; i++) {
+        if (values[i] == 11) {
+            if (mainTotal + 11 > 21) {
+
+            }
+            // do ace logic
+        } else {
+            mainTotal += values[i];
+        }
+    }
+    if (!areAcesFine) {
+        allValues[0] = mainTotal;
+    }
+    return allValues;
+}
+
+// This method runs a single mini-game of blackjack - using random number generation and not an actual deck
+void blackjack() {
+    Session debugSession;
+    debugSession.health = 10;
+    debugSession.balance = 1000;
+    debugSession.score = 50;
+    session = debugSession;
+    clear_console();
+    display_game_data();
+    puts("");
+    draw_blackjack_logo();
+    puts("Welcome to BlackJack - 21, in order to play you must either gamble your well-being or your monetary possesions. Both you and the dealer are dealt 2 cards, you may either get another card or stay with the cards you have. Whoever has the higher cumulative values of cards is the winner (but dont go over 21).");
+    printf("Would you like to gamble your well-being (1) or a portion of your balance (2): ");
+    int opt;
+    scanf("%d", &opt);
+    if (opt == 1 && ) {
+        if (session.health >= 2) {
+
+        } else {
+            // doesnt have enough health, kick outta blackjack
+        }
+    } else if (opt == 2)
+    int cardValues[52] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11};
+    char **dealer_cards = calloc(2, sizeof(char*));
+    int *dealer_values = calloc(2, sizeof(int));
+    int dealer_total_cards = 2;
+    char **player_cards = calloc(2, sizeof(char*));
+    int *player_values = calloc(2, sizeof(int));
+    int player_total_cards = 2;
+    if (dealer_cards == NULL || player_cards == NULL || dealer_values == NULL || player_values == NULL) {
+        puts("There was an error allocating memory");
+        exit(1);
+    }
+    int dealer_first = rand() % 52;
+    int dealer_second = rand() % 52;
+    int player_first = rand() % 52;
+    int player_second = rand() % 52;
+    get_card(player_first);
+    get_card(player_second);
+    get_card(dealer_first);
+    get_card(dealer_second);
+    dealer_values[0] = cardValues[dealer_first];
+    dealer_values[1] = cardValues[dealer_second];
+    player_values[0] = cardValues[player_first];
+    player_values[1] = cardValues[player_second];
+    player_cards[0] = get_card(player_first);
+    player_cards[1] = get_card(player_second);
+    dealer_cards[0] = get_card(dealer_first);
+    dealer_cards[1] = get_card(dealer_second);
+    int *total_dealer_value = get_total_points(dealer_values, dealer_total_cards);
+    int *total_player_value = get_total_points(player_values, player_total_cards);
+    printf("Your hand: ");
+    for (int i = 0; i < player_total_cards; i++) printf("%s | ", player_cards[i]);
+    //printf("Total Points: 
+    free(dealer_values);
+    free(player_values);
+    free(total_dealer_value);
+    free(total_player_value);
+    for (int i = 0; i < dealer_total_cards; i++) free(dealer_cards[i]);
+    for (int i = 0; i < player_total_cards; i++) free(player_cards[i]);
+    free(dealer_cards);
+    free(player_cards);
+}
+
 // This method will prompt the user to save/quit the game, if they choose not to then the game will continue
 void intermission() {
     clear_console();
@@ -314,6 +433,20 @@ void intermission() {
 // Like the name states, this method clears the command line interface
 void clear_console() {
     puts("\x1B[1;1H\x1B[2J");
+}
+
+// This method displays an ascii art representation of a title for the BlackJack mini-game
+void draw_blackjack_logo() {
+    puts(" ▄▄▄▄    ██▓    ▄▄▄       ▄████▄   ██ ▄█▀▄▄▄██▀▀▀▄▄▄       ▄████▄   ██ ▄█▀");
+    puts("▓█████▄ ▓██▒   ▒████▄    ▒██▀ ▀█   ██▄█▒   ▒██  ▒████▄    ▒██▀ ▀█   ██▄█▒ ");
+    puts("▒██▒ ▄██▒██░   ▒██  ▀█▄  ▒▓█    ▄ ▓███▄░   ░██  ▒██  ▀█▄  ▒▓█    ▄ ▓███▄░ ");
+    puts("▒██░█▀  ▒██░   ░██▄▄▄▄██ ▒▓▓▄ ▄██▒▓██ █▄▓██▄██▓ ░██▄▄▄▄██ ▒▓▓▄ ▄██▒▓██ █▄ ");
+    puts("░▓█  ▀█▓░██████▒▓█   ▓██▒▒ ▓███▀ ░▒██▒ █▄▓███▒   ▓█   ▓██▒▒ ▓███▀ ░▒██▒ █▄");
+    puts("░▒▓███▀▒░ ▒░▓  ░▒▒   ▓▒█░░ ░▒ ▒  ░▒ ▒▒ ▓▒▒▓▒▒░   ▒▒   ▓▒█░░ ░▒ ▒  ░▒ ▒▒ ▓▒");
+    puts("▒░▒   ░ ░ ░ ▒  ░ ▒   ▒▒ ░  ░  ▒   ░ ░▒ ▒░▒ ░▒░    ▒   ▒▒ ░  ░  ▒   ░ ░▒ ▒░");
+    puts(" ░    ░   ░ ░    ░   ▒   ░        ░ ░░ ░ ░ ░ ░    ░   ▒   ░        ░ ░░ ░ ");
+    puts(" ░          ░  ░     ░  ░░ ░      ░  ░   ░   ░        ░  ░░ ░      ░  ░   ");
+    puts("      ░                  ░                                ░               ");
 }
 
 // This method displays the title of the game, in an ascii art representation 
